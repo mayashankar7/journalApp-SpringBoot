0575304d0a9a7be180b9f07cc1d50d7747e87af1
Maya Shankar Jha <mayashankarjha301@gmail.com>
2026-09-08 23:31:20 +0530

    Mis....Update

--- src/main/java/com/mayasmethods/journalApp/service/JournalEntryService.java
+++ src/main/java/com/mayasmethods/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.mayasmethods.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +21,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String username) {
-        User user = userService.findByUsername(username);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try{
+            User user = userService.findByUsername(username);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            user.setUsername(null);
+            userService.saveEntry(user);
+        }catch(Exception e){
+            System.out.println(e);
+            throw new RuntimeException("Error saving journal entry: " + e.getMessage());
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
